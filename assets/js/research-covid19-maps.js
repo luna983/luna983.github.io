1736093b89ab187c5bc7b440427dfd9129cd81d1
luna983 <yue.huang.luna@gmail.com>
2020-06-17 23:07:41 -0700

    working for single graph

--- assets/js/research-covid19-maps.js
+++ assets/js/research-covid19-maps.js
@@ -1,62 +1,125 @@
-// specify where to add SVG
-var svg_id = "div#research-covid19-maps";
+function showN(circle, delay, duration, rscale) {
+  circle.transition()
+        .delay(delay)
+        .duration(duration)
+        .attr("r", function(d) {return Math.sqrt(d.n) * rscale;});
+  d3.select(nav_id)
+    .select('td#n')
+    .transition()
+    .delay(delay)
+    .style("opacity", 1);
+  d3.select(nav_id)
+    .select('td#p')
+    .transition()
+    .delay(delay)
+    .style("opacity", 0.5);
+};
+
+function showP(circle, delay, duration, rscale) {
+  circle.transition()
+        .delay(delay)
+        .duration(duration)
+        .attr("r", function(d) {return Math.sqrt(d.p) * rscale;});
+  d3.select(nav_id)
+    .select('td#n')
+    .transition()
+    .delay(delay)
+    .style("opacity", 0.5);
+  d3.select(nav_id)
+    .select('td#p')
+    .transition()
+    .delay(delay)
+    .style("opacity", 1);
+};
+
+// **** constant for all countries ****
 
 // specify size
 var width = 900;
 var height = 500;
 
+// specify interactive transition speed
+var delay = 1500;
+var duration = 3000;
+
+// specify color palette
+var circle_color = "#800000";
+
+// **** country specific params ****
+
+// specify country
+var countries = ["USA", "CHN", "FRA", "ITA", "KOR", "IRN"];
 // specify legend size
-var rscale = 0.001;
+var rscales = [0.2, 0.2, 0.2, 0.2, 0.2, 0.2];
+// specify the coordinate center
+var coord_centers = [[-96, 37], [104, 35], [2, 46], [12, 41], [127, 35], [53, 32]];
+// specify the projection scale
+var proj_scales = [800, 800, 800, 800, 800, 800];
 
-// initialize the SVG
-var svg = d3.select(svg_id)
-			      .append("svg")
-            // position the graph appropriately
-			      .attr("preserveAspectRatio", "xMinYMin meet")
-			      // dynamically adjust box size
-			      .attr("viewBox", "0 0 " + width + " " + height)
-			      .classed("svg-content", true);
+// loop over countries
+for (var i = 0; i < 1; i++) {
+  console.log("country: " + countries[i])
+  console.log("rscale: " + rscales[i])
+  console.log("coord_center: " + coord_centers[i])
+  console.log("proj_scale: " + proj_scales[i])
 
-// projection
-var projection = d3.geoMercator()  // specify crs
-				           .center([-96, 37])  // specify coordinate center
-				           .scale([800])  // specify scale
-				           .translate([width / 2, height / 2]);  // center
-// path generator
-var path = d3.geoPath().projection(projection);
+  // specify where to add SVG
+  var svg_id = "div#research-covid19-maps-" + countries[i];
+  var nav_id = "div#research-covid19-nav-" + countries[i];
 
-// load data
-var geoms = d3.json("/assets/data/research-covid19-maps/USA.geojson");
-var circles = d3.csv("/assets/data/research-covid19-maps/USA.csv");
+  // initialize the SVG
+  var svg = d3.select(svg_id)
+  			      .append("svg")
+              // position the graph appropriately
+  			      .attr("preserveAspectRatio", "xMinYMin meet")
+  			      // dynamically adjust box size
+  			      .attr("viewBox", "0 0 " + width + " " + height)
+  			      .classed("svg-content", true);
 
-Promise.all([geoms, circles]).then(function(data) {    
-  // draw map
-  svg.selectAll("path")
-     .data(data[0].features)
-     .enter()
-     .append("path")
-     .attr("class", "adm-geom")
-     .attr("d", path)
-     .attr("fill", "#fff")
-     .attr("stroke", "#777")
-     .attr("stroke-width", 2),
-  svg.selectAll("circle")
-     .data(data[1])
-     .enter()
-     .append("circle")
-     .attr("class", "adm-circle")
-     .attr("cx", function(d) {return projection([d.lon, d.lat])[0];})
-     .attr("cy", function(d) {return projection([d.lon, d.lat])[1];})
-     .attr("r", function(d) {return d.p * rscale;})
-     .attr("fill", "#800000")
-     .attr("opacity", 0.3);
-});
+  // projection
+  var projection = d3.geoMercator()  // specify crs
+  				           .center(coord_centers[i])  // specify coordinate center
+  				           .scale(proj_scales[i])  // specify scale
+  				           .translate([width / 2, height / 2]);  // center
+  // path generator
+  var path = d3.geoPath().projection(projection);
 
-function showN() {
-  circles.then(function(data) {
-    svg.select()
-       .transition()
-       .duration(1000)
-       .attr("r", function(d) {return d.p * rscale;});
+  // load data
+  var path_data = d3.json(
+    "/assets/data/research-covid19-maps/" + countries[i] + ".geojson");
+  var circle_data = d3.csv(
+    "/assets/data/research-covid19-maps/" + countries[i] + ".csv");
+
+  Promise.all([path_data, circle_data]).then(function(data) {
+    // draw map
+    svg.selectAll("path")
+       .data(data[0].features)
+       .enter().append("path")
+       .attr("d", path)
+       .attr("fill", "#fff")
+       .attr("stroke", "#777")
+       .attr("stroke-width", 2);
+    // draw circles
+    var circle = svg.selectAll("circle")
+                    .data(data[1])
+                    .enter().append("circle");
+    circle.attr("cx", function(d) {return projection([d.lon, d.lat])[0];})
+          .attr("cy", function(d) {return projection([d.lon, d.lat])[1];})
+          .attr("r", function(d) {return Math.sqrt(d.p) * rscales[i];})
+          .attr("fill", circle_color)
+          .attr("opacity", 0.3);
+    return circle;
+  }).then(function(circle) {
+    // transition
+    showN(circle, delay, duration, rscales[i]);
+    return circle;
+  }).then(function(circle) {
+    // define interaction
+    d3.select(nav_id)
+      .select('td#n')
+      .on('click', function() {showN(circle, 0, duration, rscales[i])});
+    d3.select(nav_id)
+      .select('td#p')
+      .on('click', function() {showP(circle, 0, duration, rscales[i])});
   });
 };
